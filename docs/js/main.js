ce23bf109d6a90565977b75540ef4a1c9f62ba22
Grayson Earle <graysonearle@gmail.com>
2018-04-08 19:07:23 -0400

    website overhaul

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,78 +1,109 @@
-(function() {
-    function getStats() {
-        $('#money-raised-holder').hide();
-        var walletAddress =
-            '442uGwAdS8c3mS46h6b7KMPQiJcdqmLjjbuetpCfSKzcgv4S56ASPdvXdySiMizGTJ56ScZUyugpSeV6hx19QohZTmjuWiM';
+// download button
+function getRelease() {
+    var defaultURL = 'https://github.com/thenewinquiry/bailbloc/releases/latest';
+    var apiURL = 'https://api.github.com/repos/thenewinquiry/bailbloc/releases/latest';
+    var userAgent = navigator.userAgent;
 
-        $.when(
-                $.getJSON('https://api.cryptonator.com/api/ticker/xmr-usd'),
-                $.getJSON('http://165.227.73.246')
-            )
-            .done(function(exchangeRate, wallet) {
-                var rate = exchangeRate[0].ticker.price;
-                var income = wallet[0].result.balance;
-                var total = (rate * income).toFixed(2);
-                $('#money-raised').text('$' + total);
-                if (total > 100) $('#money-raised-holder').fadeIn();
-            })
-            .fail(function(e) {
-                console.log('error', e);
-            });
+    if (/Mac/.test(userAgent)) {
+        $('#download-button').text('Click To Download For Mac');
+    } else if (/Windows/.test(userAgent)) {
+        $('#download-button').text('Click To Download For Windows');
+    }
 
-        $.getJSON('https://api.xmrpool.net/miner/' + walletAddress + '/stats/allWorkers').done(function(workers) {
-            var totalWorkers = Object.keys(workers).length - 1;
-            $('#active-users').text(totalWorkers);
-            if (totalWorkers > 1) $('#money-raised-holder').fadeIn();
+    $.getJSON(apiURL, function(release) {
+        var url = defaultURL;
+        var assets = release.assets.map(function(a) {
+            return a.browser_download_url;
         });
-
-        setTimeout(getStats, 30000);
-    }
-
-
-
-    // take to download page after clicking link
-    $('#download-button').click(function() {
-        setTimeout(gotoDownloadPage, 1000);
-        
-    });
-
-    function gotoDownloadPage() {
-      window.location.href = "download.html";
-    }
-
-
-
-    function getRelease() {
-        var defaultURL = 'https://github.com/thenewinquiry/bailbloc/releases/latest';
-        var apiURL = 'https://api.github.com/repos/thenewinquiry/bailbloc/releases/latest';
-        var userAgent = navigator.userAgent;
 
         if (/Mac/.test(userAgent)) {
-            $('#download-button').text('Click To Download For Mac');
+            url = assets.find(function(a) {
+                return a.indexOf('.dmg') > -1;
+            });
         } else if (/Windows/.test(userAgent)) {
-            $('#download-button').text('Click To Download For Windows');
+            url = assets.find(function(a) {
+                return a.indexOf('.exe') > -1;
+            });
         }
 
-        $.getJSON(apiURL, function(release) {
-            var url = defaultURL;
-            var assets = release.assets.map(function(a) {
-                return a.browser_download_url;
-            });
+        $('#download-button').attr('href', url);
+    });
+}
 
-            if (/Mac/.test(userAgent)) {
-                url = assets.find(function(a) {
-                    return a.indexOf('.dmg') > -1;
-                });
-            } else if (/Windows/.test(userAgent)) {
-                url = assets.find(function(a) {
-                    return a.indexOf('.exe') > -1;
-                });
-            }
+getRelease();
 
-            $('#download-button').attr('href', url);
-        });
+// take to download page after clicking link
+$('#download-button').click(function() {
+    setTimeout(gotoDownloadPage, 1000);
+
+});
+
+function gotoDownloadPage() {
+    window.location.href = "download.html";
+}
+
+// simulation
+var worker;
+$('#sim-start').on('click', function() {
+    if (worker) {
+        worker.terminate();
     }
+    $('.sim-status').show();
+    $('.sim-results').hide();
+    worker = new Worker('js/sim.js');
+    worker.onmessage = function(m) {
+        var results = m.data;
+        $('#sim-raised').text(results.raised.toLocaleString());
+        $('#sim-released').text(results.released.toLocaleString());
+        $('#sim-reduced').text(results.reduced.toLocaleString());
+        $('.sim-results').show();
+        $('.sim-status').hide();
+    };
+    worker.postMessage({
+        nMonths: $('input[name=months]').val(),
+        nMiners: $('input[name=users]').val()
+    });
+});
+$('input').on('keydown', function(ev) {
+    if (ev.keyCode == 13) {
+        $('#sim-start').click();
+    }
+});
 
-    getRelease();
-    // getStats();
-})();
+
+// social
+
+function facebook() {
+
+    var raised = $('#sim-raised').text();
+    var released = $('#sim-released').text();
+    var reduced = $('#sim-reduced').text();
+
+    var users = $("input[name=users]").val();
+    var months = $("input[name=months]").val();
+
+    var sentence = "If " + users + " ran Bail Bloc for " + months + "months we could raise $" + raised + "and get " + reduced + " cases dismissed.";
+
+    popUp("https://www.facebook.com/dialog/feed?app_id=140586622674265&link=http://bailbloc.thenewinquiry.com&name=" + sentence + "&redirect_uri=http%3A%2F%2Fs7.addthis.com%2Fstatic%2Fpostshare%2Fc00.html", 900, 600);
+}
+
+function twitter() {
+
+    var raised = $('#sim-raised').text();
+    var released = $('#sim-released').text();
+    var reduced = $('#sim-reduced').text();
+
+    var users = $("input[name=users]").val();
+    var months = $("input[name=months]").val();
+
+    var sentence = "If " + users + " ran Bail Bloc for " + months + " months we could raise $" + raised + " and get " + reduced + " cases dismissed. Join the Bloc %23BAILBLOC";
+
+    popUp("https://twitter.com/intent/tweet?text=" + sentence + "&url=http://bailbloc.thenewinquiry.com", 704, 260);
+}
+
+// create pop up windows
+function popUp(url, _width, _height) {
+    newwindow = window.open(url, 'Join the Bloc!', 'height=' + _height + ',width=' + _width);
+    if (window.focus) { newwindow.focus() }
+    return false;
+}
